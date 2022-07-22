811d3efe4973dbdb2b4a5f784cf9bfc8df742877
Vegadrone <spenaantonino@gmail.com>
2022-07-22 17:53:30 +0200

    probles with flags src

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="container">
-    <div class="row ">
-        <div class="col-4">
-            <ul>
-                <li>{{singleFilm.title}} - {{singleFilm.original_title}} - {{singleFilm.original_language}} - {{singleFilm.vote_average}}</li>
-            </ul>
-        </div>
+    <div class="row">
+      <div class="col-4">
+        <ul>
+          <li>{{ singleFilm.title }}</li>
+          <li>{{ singleFilm.original_title }}</li>
+          <li>{{ singleFilm.original_language }}</li>
+          <li>{{ singleFilm.vote_average }}</li>
+        </ul>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-    props:["singleFilm"]
-}
+  props: ["singleFilm"],
+};
 </script>
 
 <style>
-
 </style>
--- src/components/TvShowCard.vue
+++ src/components/TvShowCard.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="container">
-    <div class="row ">
-        <div class="col-4">
-            <ul>
-                 <li>{{singleTvShow.name}} - {{singleTvShow.original_name}} - {{singleTvShow.original_language}} - {{singleTvShow.vote_average}}</li>
-            </ul>
-        </div>
+    <div class="row">
+      <div class="col-4">
+        <ul>
+          <li>{{ singleTvShow.name }}</li>
+          <li>{{ singleTvShow.original_name }}</li>
+          <li><img :src="`../assets/flags/${singleTvShow.original_language}.png`" alt="language flag"></li>
+          <li>{{ singleTvShow.vote_average }}</li>
+        </ul>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: ["singleTvShow"],
 
-    props:[
-        'singleTvShow'
-    ]
+  methods:{
 
-}
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss">
+    img{
+        width: 100px;
+        height: 100px;
+    }
 </style>
